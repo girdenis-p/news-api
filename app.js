e4cf0fe53ec0a83bc17509f94271f77812c96c35
girdenis-p <girdenis.p@gmail.com>
2023-02-20 12:23:44 +0000

    Implement 404 handling to send message

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 
 app.get('/api/topics', getTopics);
 
+app.use((req, res, next) => {
+  res.status(404).send({msg: `Endpoint ${req.url} not found!`});
+})
 app.use(handle500StatusCodes);
 
 module.exports = app;
